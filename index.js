dd7bdb15b8b412af4a6399ec3a437777ea04d537
Thales Lourençon <109592406+thales4110@users.noreply.github.com>
2023-05-30 13:07:15 -0300

    Create routes and pages

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-const exp = require("constants")
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
 
-app.get("/", (req,res)=>{
-    res.send("hello!!!!!")
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+//Rotes
+app.get("/", (req,res) => {
+    res.render("index")   
+})
+
+app.get("/question", (req,res) => {
+    res.render("question")
+})
+
+app.post("/savequestion", (req,res) => {
+    let title = req.body.title
+    let description = req.body.description
+    res.send("Title: " + title + " " + "Description: " + description)
 })
 
 
-app.listen(5000, ()=>{
+
+app.listen(5000, () => {
     console.log("http://localhost:5000")
 })
